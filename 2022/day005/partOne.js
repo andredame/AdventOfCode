2ea4a1227dde5577c4b09f3b000b908028affa4e
Andre Dame Fabiao <andre.fabiao@outlook.com>
2024-02-27 15:42:32 -0300

    part  One Done 2022 day 5

--- 2022/day005/partOne.js
+++ 2022/day005/partOne.js
@@ -1,3 +1,4 @@
+// For this part, I thought to implement a brute force algorithm that distributes elements into other stacks.
 import fs, { copyFileSync } from 'fs';
 
 const input = fs.readFileSync('2022/day005/input.txt', 'utf8').replace(/\r/g, '').split('\n');
@@ -5,7 +6,10 @@ const input = fs.readFileSync('2022/day005/input.txt', 'utf8').replace(/\r/g, ''
 let [stackInput, movements ] = [input.slice(0, input.indexOf('')), input.slice(input.indexOf('') + 1)];
 
 
-var NUMBEROFSTACKS= stackInput[stackInput.length-1].split('  ').map(Number).length;
+var NUMBEROFSTACKS= stackInput[stackInput.length-1].split('')
+
+
+
 stackInput.pop()
 
 
@@ -13,36 +17,55 @@ stackInput.pop()
 function main(){
 
     let stacks= new Map();
-    stack(stacks);
-    fillStacks(stacks);
-    console.log(stacks);
+    let numberToIndex=new Map();
+    
+    createMap(stacks,numberToIndex);
+    console.log(stacks)
+
+    fillStacks(stacks); 
+    console.log(numberToIndex)
+    movements.forEach(line =>{
+        let phrase=line.split(' ')
+        let [amount,source,target]=[parseInt(phrase[1]),numberToIndex.get(parseInt(phrase[3])),numberToIndex.get(parseInt(phrase[5]))];
+        console.log(stacks,source,target)
+
+        for(let i=0;i<amount;i++){
+            stacks.get(target).push(stacks.get(source).pop());
+        }
+
+    });
+    
+    let outcome='';
+    stacks.forEach(array=>{
+        outcome+=array.pop();
+    })
+    console.log(outcome)
+    
+
+
 }
 
 //Fill the stacks with the input for the first time 
 const fillStacks= (stacks) => {
     for(let i=stackInput.length-1;i>=0;i--){
+        
         stackInput[i].split('').forEach((element,index) => {
             if(element !== ' ' && element !== '[' && element !== ']'){
-                if(index%NUMBEROFSTACKS ==0 ){stacks.get(NUMBEROFSTACKS).push(element)}
-                else{stacks.get(index%NUMBEROFSTACKS).push(element)}
+                stacks.get(index).push(element)
             }
         });
-
     }
 }
 
-//create the stacks with the number of stacks given
-const stack= (stacks) => {
-    for(let i=1; i<=NUMBEROFSTACKS; i++){stacks.set(i,[]);}
+const createMap= (stacks,numberToIndex) => {
+    let count=1;
+    for(let i=0; i<NUMBEROFSTACKS.length; i++){ 
+        if(NUMBEROFSTACKS[i]!=' '){
+            numberToIndex.set(count,i)
+            stacks.set(i,[])
+            count++
+        }
+    }
 }
-
-
-//Here is where the magic happens
-function pileUp(stack){
-
-    
-
-}
-
 
 main();
